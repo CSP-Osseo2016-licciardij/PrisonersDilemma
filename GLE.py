942a92def982c6411182c6a8f2c608884647fe42
ElsaAthiley <athil002@apps.district279.org>
2017-01-03 13:13:26 -0600

    Add files via upload

---
+++ GLE.py
@@ -0,0 +1,7 @@
+team_name = 'GLE'
+strategy_name = 'Always Betray'
+strategy_description = '''Betray every round because it is the most logical move'''
+
+    
+def move(my_history, their_history, my_score, their_score):
+    return 'b'
