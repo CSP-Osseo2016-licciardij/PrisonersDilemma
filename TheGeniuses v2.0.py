07a85dda211ce54ddad2f9054df663c4d6e9c7ff
malay001 <malay001@apps.district279.org>
2017-01-04 12:14:00 -0600

    Add files via upload
    even better than before hahaaaaaaaaaaaaaaaaaaaaa

---
+++ TheGeniuses v2.0.py
@@ -0,0 +1,8 @@
+team_name = 'The Geniuses' 
+strategy_name = 'All Calculated'
+strategy_description = 'Uses a completely intricate and well thought-out algorithm to generate the best result.(picks a random option)'
+    
+def move(my_history, their_history, my_score, their_score):
+   
+    return 'c' or 'b'       
+    #random stuff lmao
